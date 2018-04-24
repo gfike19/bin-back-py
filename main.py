5c2794561a5aaee70d02ee71fb7c4b69c9ecac5d
Gail Fike <gfike19@gmail.com>
2018-04-24 15:03:25 -0500

    did things

--- main.py
+++ main.py
@@ -23,14 +23,26 @@ def writeLinesRTL(draw, msg, img_size, txt_color, font, text_size):
             if idx > leng - 1:
                 idx = 0
 
+def writeLinesTTB(draw, msg, img_size, txt_color, font, text_size):
+    leng = len(msg)
+    idx = 0
+    for x in range(0, img_size[0], text_size):
+        for y in range(0, img_size[1], text_size):
+            draw.text((x,y),msg[idx], txt_color, font=font)
+            idx += 1
+            if idx > leng - 1:
+                idx = 0
+
 name = input("What will be the name of this image be?: ")
 wid = int(input("What is the width of this image?: "))
 ht = int(input("What is the height of this image?: "))
 img_size = (wid, ht)
 txt_color = input("What's the color of the text?: ")
 back = input("What is the background color for this immage?: ")
-msg = input("What is the message to be converted?: ")
-msg = getBinMsg(msg)
+msg = input("What is the message?: ")
+bin_stat = input("Binary? (y/n) ")
+if bin_stat == "y":
+    msg = getBinMsg(msg)
 text_size = int(input("What size will the font be?: "))
 
 e = ""
@@ -42,6 +54,7 @@ try:
     font = ImageFont.truetype("arial.ttf", text_size)
     # draw.text((x, y),"Sample Text",(r,g,b))
     writeLinesRTL(draw, msg, img_size, txt_color, font, text_size)
+    #writeLinesTTB(draw, msg, img_size, txt_color, font, text_size)
     name += ".jpg"
     img.save(name)
 except Exception as e:
